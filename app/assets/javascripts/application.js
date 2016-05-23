59625796cd5d2f5bd3286589239920820ed3fa34
Hernan <hernanprovostev@outlook.com>
2016-05-22 22:30:06 -0400

    Fix imagen de la semana 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,17 @@
 //= require_tree .
 
 var someJavascriptStuff = {};
+
+$(function() {
+  var $slideactive, $slideshow;
+  $slideshow = $('.gallery ul');
+  $slideactive = $slideshow.find('li.each').first().addClass('active').show();
+  $('.direction .next').click(function() {
+    $slideactive = $slideshow.find('li.active').next();
+    if (!$slideactive.size()) {
+      $slideactive = $slideshow.find('li.each').first();
+    }
+    $slideshow.find('li.active').removeClass('active');
+    $slideactive.addClass('active');
+  });
+});
